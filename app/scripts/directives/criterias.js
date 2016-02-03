bda4c73c4f62b10dd8c2c1cf844572f9ce43fda1
didjoman <alexrupp@free.fr>
2016-02-03 15:41:29 +0100

    Clean criterias

--- app/scripts/directives/criterias.js
+++ app/scripts/directives/criterias.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module('Criterias', [])
-.directive('criterias', function($parse, $window) {
+.directive('criterias', function($parse, $window, CriteriasSvc) {
    return{
       restrict:'EA',
       scope: {
@@ -15,31 +15,10 @@ angular.module('Criterias', [])
       		//scope.data.criteria = attrs.criteria;
 
       		scope.formtype = attrs.formtype;
-      	    scope.trad = {
-		      'autocar_count' : 'Autocars',
-		      'bus_count' : 'Bus',
-		      'citelib_count' : 'Citelibs',
-		      'gsm_2g_count' : '2G',
-		      'gsm_3g_count' : '3G',
-		      'gsm_4g_count' : '4G',
-		      'sncf_count' : 'SNCF',
-		      'tram_count' : 'Tram',
-		      'cyclelane_length' : 'Vélo voies',
-		    };
+      	    scope.getTrad = CriteriasSvc.getTrad;
 
 		    // Init dimensions
-		    scope.dimensions = {       
-                        'name' : true,            
-                        'autocar_count' : false,
-                        'bus_count' : false,
-                        'citelib_count' : false,
-                        'gsm_2g_count' : false,
-                        'gsm_3g_count' : false,
-                        'gsm_4g_count' : false,
-                        'sncf_count' : false,
-                        'tram_count' : false,
-                        'cyclelane_length' : false,
-            };
+		    scope.dimensions = CriteriasSvc.getDimensions();
 
             scope.getKey = function(i){
 		      return Object.keys(scope.dimensions)[i];
